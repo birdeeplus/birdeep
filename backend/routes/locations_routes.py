ad8bbe2b7195a484e373653030818a9a879b5d58
Mar Iborra ;) <mariborra2003@gmail.com>
2025-02-28 02:48:26 +0100

    correcciones errores paginas localización

--- backend/routes/locations_routes.py
+++ backend/routes/locations_routes.py
@@ -1,7 +1,7 @@
 #/routes/locations_routes.py
 
 from flask import Blueprint
-from controllers.locations_controller import insert_new_location, query_locations, update_location, delete_location, get_location_by_id
+from controllers.locations_controller import insert_new_location, query_locations, update_location, delete_location, get_location_by_id, get_recorders_by_location
 
 locations_bp = Blueprint('locations', __name__)
 
@@ -10,3 +10,4 @@ locations_bp.add_url_rule('/api/v1/locations', view_func=query_locations, method
 locations_bp.add_url_rule('/api/v1/locations/<int:id_location>', view_func=update_location, methods=['PUT'])
 locations_bp.add_url_rule('/api/v1/locations/<int:id_location>', view_func=delete_location, methods=['DELETE'])
 locations_bp.add_url_rule('/api/v1/locations/<int:id_location>', view_func=get_location_by_id, methods=['GET'])
+locations_bp.add_url_rule('/api/v1/locations/<int:id_location>/recorders', view_func=get_recorders_by_location, methods=['GET'])
