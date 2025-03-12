1391893a7e6a54e015bc6749eef9ee8070c059b3
Diego García-Romero <119662400+didi-yf@users.noreply.github.com>
2025-03-12 21:44:42 +0100

    Implementacion de las paginas para el admin

--- frontend/src/components/navbars/Navbar_busqueda.jsx
+++ frontend/src/components/navbars/Navbar_busqueda.jsx
@@ -11,28 +11,30 @@ import LogoutModal from "../Logout";
 export default function Navbar({ toggleLanguage, language }) {
     const [menuOpen, setMenuOpen] = useState(false);
     const [loginOpen, setLoginOpen] = useState(false);
-    const [logoutOpen, setLogoutOpen] = useState(false); // Estado para abrir el modal de logout
+    const [logoutOpen, setLogoutOpen] = useState(false);
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     const [isAdmin, setIsAdmin] = useState(false);
 
-    // Función para cerrar el menú si se hace clic fuera de él
     const closeMenuOutsideClick = (event) => {
         if (event.target.id === "menuOverlay") {
             setMenuOpen(false);
         }
     };
 
-    // Función para cerrar el modal de login si se hace clic fuera de él
     const closeLoginOutsideClick = (event) => {
         if (event.target.id === "loginOverlay") {
             setLoginOpen(false);
         }
     };
 
-    // Opciones del menú según el idioma
     const menuOptions = {
         en: { locations: "Locations", recorders: "Recorders", recordings: "Recordings" },
         es: { locations: "Ubicaciones", recorders: "Grabadoras", recordings: "Grabaciones" },
+    };
+
+    const adminOptions = {
+        en: { microphones: "Microphones", processors: "Processors" },
+        es: { microphones: "Micrófonos", processors: "Procesadores" },
     };
 
     const handleLoginSuccess = (isAdmin) => {
@@ -46,7 +48,7 @@ export default function Navbar({ toggleLanguage, language }) {
         localStorage.removeItem("is_admin");
         setIsLoggedIn(false);
         setIsAdmin(false);
-        setLogoutOpen(false); // Cerrar el modal después del logout
+        setLogoutOpen(false);
     };
 
     useEffect(() => {
@@ -60,14 +62,12 @@ export default function Navbar({ toggleLanguage, language }) {
 
     return (
         <nav className="w-full flex justify-between items-center py-2 px-2 fixed top-0 z-50">
-            {/* Logo con Link a la página principal */}
             <div className="flex items-center pl-10">
                 <Link href="/" passHref>
                     <Image src="/photos/logo.png" alt="Logo" width={85} height={85} className="cursor-pointer" />
                 </Link>
             </div>
 
-            {/* Barra de búsqueda */}
             <div className="flex-grow mx-10">
                 <input
                     type="text"
@@ -76,18 +76,12 @@ export default function Navbar({ toggleLanguage, language }) {
                 />
             </div>
 
-            {/* Controles de Usuario */}
             <div className="flex items-center gap-5">
-                {/* Botón de Cambio de Idioma */}
                 <LanguageToggleButton toggleLanguage={toggleLanguage} language={language} />
 
-                {/* Si el usuario está logueado como Admin, solo mostrar "Admin" */}
                 {isLoggedIn && isAdmin ? (
                     <div className="flex items-center gap-3">
-                        <span
-                            className="text-green-500 font-bold cursor-pointer"
-                            onClick={() => setLogoutOpen(true)} // Abrir el modal de logout al hacer clic
-                        >
+                        <span className="text-green-500 font-bold cursor-pointer" onClick={() => setLogoutOpen(true)}>
                             Admin
                         </span>
                     </div>
@@ -97,13 +91,11 @@ export default function Navbar({ toggleLanguage, language }) {
                     </button>
                 )}
 
-                {/* Menú Hamburguesa */}
                 <button onClick={() => setMenuOpen(true)} className="pr-10 rounded-full">
                     <Image src="/iconos/menu.png" alt="Menu" width={33} height={33} />
                 </button>
             </div>
 
-            {/* Menú Desplegable con Animación */}
             <AnimatePresence>
                 {menuOpen && (
                     <motion.div
@@ -122,7 +114,6 @@ export default function Navbar({ toggleLanguage, language }) {
                             exit={{ x: "100%" }}
                             transition={{ duration: 0.6, ease: "easeInOut" }}
                         >
-                            {/* Botón de Cierre */}
                             <button
                                 onClick={() => setMenuOpen(false)}
                                 className="absolute top-4 right-4 text-xl font-bold"
@@ -130,7 +121,6 @@ export default function Navbar({ toggleLanguage, language }) {
                                 ✕
                             </button>
 
-                            {/* Opciones del Menú */}
                             <ul className="ml-10 mt-[100] space-y-6">
                                 {Object.keys(menuOptions[language]).map((key) => (
                                     <li key={key}>
@@ -144,13 +134,26 @@ export default function Navbar({ toggleLanguage, language }) {
                                         </Link>
                                     </li>
                                 ))}
+
+                                {isAdmin &&
+                                    Object.keys(adminOptions[language]).map((key) => (
+                                        <li key={key}>
+                                            <Link href={`/general/${key}_general`}>
+                                                <motion.span
+                                                    className="InterRegular font-bold text-black-500 cursor-pointer hover:text-gray-600 transition duration-300 block"
+                                                    whileHover={{ scale: 1.1 }}
+                                                >
+                                                    {adminOptions[language][key]}
+                                                </motion.span>
+                                            </Link>
+                                        </li>
+                                    ))}
                             </ul>
                         </motion.div>
                     </motion.div>
                 )}
             </AnimatePresence>
 
-            {/* Modal de Login */}
             <AnimatePresence>
                 {loginOpen && (
                     <motion.div
@@ -162,16 +165,11 @@ export default function Navbar({ toggleLanguage, language }) {
                         exit={{ opacity: 0 }}
                         transition={{ duration: 0.5, ease: "easeInOut" }}
                     >
-                        <LoginForm 
-                            language={language} 
-                            onClose={() => setLoginOpen(false)} 
-                            onLoginSuccess={handleLoginSuccess} 
-                        />
+                        <LoginForm language={language} onClose={() => setLoginOpen(false)} onLoginSuccess={handleLoginSuccess} />
                     </motion.div>
                 )}
             </AnimatePresence>
 
-            {/* Modal de Logout */}
             <AnimatePresence>
                 {logoutOpen && (
                     <motion.div
@@ -182,11 +180,7 @@ export default function Navbar({ toggleLanguage, language }) {
                         exit={{ opacity: 0 }}
                         transition={{ duration: 0.5, ease: "easeInOut" }}
                     >
-                        <LogoutModal
-                            language={language}
-                            onClose={() => setLogoutOpen(false)}
-                            onLogoutConfirm={handleLogout}
-                        />
+                        <LogoutModal language={language} onClose={() => setLogoutOpen(false)} onLogoutConfirm={handleLogout} />
                     </motion.div>
                 )}
             </AnimatePresence>
