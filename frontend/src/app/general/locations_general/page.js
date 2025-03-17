28eb0ed4d38a0265324700ef7351e5e3551f3a16
lucas <lucasgarcia.exe@gmail.com>
2025-03-17 01:00:00 +0100

    arreglo location

--- frontend/src/app/general/locations_general/page.js
+++ frontend/src/app/general/locations_general/page.js
@@ -5,6 +5,7 @@ import { MapContainer, TileLayer, Marker, Tooltip } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import Navbar from "../../../components/navbars/Navbar_general";
+import AddLocationButton from "../../../components/AddLocationButton";
 
 const blackIcon = new L.Icon({
     iconUrl: "/iconos/map-marker-black.png",
@@ -16,23 +17,48 @@ const blackIcon = new L.Icon({
 export default function LocationsGeneral() {
     const [language, setLanguage] = useState("en");
     const [locations, setLocations] = useState([]);
+    const [isAdmin, setIsAdmin] = useState(false);
     const router = useRouter();
 
     const toggleLanguage = () => {
         setLanguage((prev) => (prev === "en" ? "es" : "en"));
     };
 
-    useEffect(() => {
+    const fetchLocations = () => {
         fetch("http://localhost:8080/api/v1/locations")
             .then((response) => response.json())
             .then((data) => setLocations(data))
             .catch((error) => console.error("Error fetching locations:", error));
+    };
+
+    const updateAdminStatus = () => {
+        const userRole = localStorage.getItem("is_admin");
+        setIsAdmin(userRole === "true"); // Verifica correctamente el valor de admin
+    };
+
+    useEffect(() => {
+        updateAdminStatus(); // Ejecutar al inicio
+        fetchLocations();
+
+        // Escuchar cambios en localStorage
+        const handleStorageChange = () => {
+            updateAdminStatus();
+        };
+
+        window.addEventListener("storage", handleStorageChange);
+
+        return () => {
+            window.removeEventListener("storage", handleStorageChange);
+        };
     }, []);
 
     return (
         <div className="relative w-full h-screen">
             {/* Navbar */}
             <Navbar toggleLanguage={toggleLanguage} language={language} />
+
+            {/* Botón de añadir localización si es admin */}
+            {isAdmin && <AddLocationButton onAddLocation={fetchLocations} />}
 
             {/* Contenedor del mapa */}
             <div className="relative w-full h-full pt-[90px]">
@@ -41,12 +67,6 @@ export default function LocationsGeneral() {
                     zoom={11}
                     className="absolute top-0 left-0 w-full h-full z-0"
                 >
-                    <style>{`
-                        .leaflet-top.leaflet-left {
-                            top: 750px !important; /* Ajusta la altura */
-                        }
-                    `}</style>
-
                     <TileLayer url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png" />
 
                     {locations.map((location) => (
